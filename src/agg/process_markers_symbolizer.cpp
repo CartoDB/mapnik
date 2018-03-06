f86a4baac216469f36378eebccf744759ff7ca1e
Rafa de la Torre <rtorre@carto.com>
2018-03-06 15:37:35 +0100

    Increase cache sampling_rate to 8
    This incidentally avoids some race conditions in visual tests. Yet, it
    should be good enough to be performant when dealing with lots of features
    with a few markers.

--- src/agg/process_markers_symbolizer.cpp
+++ src/agg/process_markers_symbolizer.cpp
@@ -241,7 +241,7 @@ private:
 	    > cached_images_;
 
     static constexpr size_t cache_size = 4096; // maximum number of images to cache. Note that the number of actual images stored depends also on sampling_rate
-    static constexpr int sampling_rate = 4; // this determines subpixel precision. The larger the value, the closer the solution will be compared to the reference but will reduce the cache hits
+    static constexpr int sampling_rate = 8; // this determines subpixel precision. The larger the value, the closer the solution will be compared to the reference but will reduce the cache hits
 };
 
 #ifdef MAPNIK_THREADSAFE
